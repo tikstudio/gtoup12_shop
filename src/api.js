59403277f71768aa722e6781c7a78821f0c31894
Tigran Muradyan <tigran.inbox@gmail.com>
2019-05-19 13:56:15 +0400

    fixed API autorization

--- src/api.js
+++ src/api.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 const CONSUMER_KEY = 'ck_3c19be5991be88176096737429edd9612b0ac80b';
 const CONSUMER_SECRET = 'cs_6e016ce51793f3b41aff2c1c729f09fb6122ec6d';
 
-// axios.defaults.baseURL = 'http://tempwp.orderwebsitenow.com';
-axios.defaults.baseURL = 'http://localhost/group12';
-// axios.defaults.headers.consumer_key = CONSUMER_KEY;
-// axios.defaults.headers.consumer_secret = CONSUMER_SECRET;
+axios.defaults.baseURL = 'https://localhost/group12';
+
+axios.interceptors.request.use((config) => {
+  const s = config.url.indexOf('?') > -1 ? '&' : '?'
+  config.url = config.url += `${s}consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`
+  return config;
+}, error => Promise.reject(error));
 
 export function getProducts() {
-  return axios.get(`/wp-json/wc/v3/products?consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`);
+  return axios.get(`/wp-json/wc/v3/products`);
 }
